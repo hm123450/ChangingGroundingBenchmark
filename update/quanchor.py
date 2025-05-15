a77e0e7b79ed90b9c186b152ea4eedb04b2171ac
hm123450 <149764448+hm123450@users.noreply.github.com>
2025-05-15 17:43:45 +0800

    Add files via upload

---
+++ update/quanchor.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import re
+# 读取CSV文件
+df = pd.read_csv('choose4/today250.csv')  # 替换'your_file.csv'为你的文件路径
+def remove_pattern(text):
+    # 使用正则表达式替换一个或多个空格后跟'%anchor_article%'为''
+    return re.sub(r'\s+%anchor_article%', '', text)
+# 假设我们要处理的列名为'ColumnName'
+column_name = 'utterance'  # 替换'ColumnName'为你的列名
+print(df[column_name])
+# 遍历列中的每个元素，检查并删除'%anchor_article%'
+df[column_name] = df[column_name].apply(lambda x: remove_pattern(x) if '%anchor_article%' in x else x)
+
+# 将更新后的DataFrame保存回CSV文件
+df.to_csv('choose4/today250_updated.csv', index=False)  # 替换'updated_file.csv'为你想要保存的新文件路径
